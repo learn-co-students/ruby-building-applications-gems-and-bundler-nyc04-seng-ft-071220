44206917f5872f72ed8154ff6c31558dbc43ef70
iannakim <dear.hyunji@gmail.com>
2020-08-15 12:25:12 -0400

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,1 +1,6 @@
 # Set up Bundler here following the instructions in the README
+# should require bundler/setup in the environment (FAILED - 1)
+# should require the default and development groups in the environment (FAILED - 2)
+
+require 'bundler/setup'
+Bundler.require(:default, :development)
